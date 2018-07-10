02675e6393853e01ad1395b535fe42eab6bd163c
Mateusz <maciejeczka95@gmail.com>
2018-07-10 10:18:39 +0200

    initial commit

--- src/main/java/app/AppConfig.java
+++ src/main/java/app/AppConfig.java
@@ -1,7 +1,6 @@
 package app;
 
 import app.DTO.InfoDto;
-import app.DTO.ResponseMsg;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
